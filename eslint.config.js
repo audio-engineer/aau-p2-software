3c8747fdb65f8161b5bbeddefc9329c6901823ac
Martin Kedmenec <martin.kedmenec@outlook.com>
2024-02-13 20:26:49 +0100

    Update typescript-eslint to 7.0.1. Migrate to flat configuration. fix #4

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import nextPlugin from "@next/eslint-plugin-next";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.all,
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+  },
+  eslintConfigPrettier,
+  {
+    ignores: ["eslint.config.js"],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          format: ["camelCase", "UPPER_CASE", "StrictPascalCase"],
+        },
+      ],
+    },
+  },
+);
