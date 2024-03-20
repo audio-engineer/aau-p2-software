aaf075f4e4e739b90ade94377f507d8aa37b0d7e
Martin Kedmenec <martin.kedmenec@outlook.com>
2024-03-20 18:06:31 +0100

    Fix #51. Update MUI cache provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import type { FC, ReactNode } from "react";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 
 const inter = Roboto({ subsets: ["latin"], weight: "500" });
 
