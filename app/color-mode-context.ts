e5c516d1184a37732cadb76ac0f3a533d4d5e5e0
Martin Kedmenec <martin.kedmenec@outlook.com>
2024-04-11 12:30:18 +0200

    Fix #105. Improve local storage

--- app/color-mode-context.ts
+++ app/color-mode-context.ts
@@ -1,5 +1,10 @@
 import { createContext } from "react";
 
+export type ColorMode = "dark" | "light";
+
+export const dark: ColorMode = "dark";
+export const light: ColorMode = "light";
+
 interface ColorModeContextProps {
   readonly toggleColorMode: () => void;
 }
